f4f11b6e56ebe32929a08e665729c3d3e3a52cac
Dichloride <104413211+Dichloride@users.noreply.github.com>
2024-10-20 17:31:47 +0800

    test(macros): complete tests under macros

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -5,8 +5,8 @@
 // Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
+// Added the attribute `macro_use` attribute.
+#[macro_use]
 mod macros {
     macro_rules! my_macro {
         () => {
